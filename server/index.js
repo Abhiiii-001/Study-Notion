c8b68323aec819246f4b9f2db50d9c798b57a016
Abhishek Kumar <114284738+Abhiiii-001@users.noreply.github.com>
2024-05-27 19:13:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,13 +29,6 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 // res.header( "Access-Control-Allow-Origin" );
-app.use(function(req, res, next) {
-	  res.header('Access-Control-Allow-Origin', "*");
-	  res.header('Access-Control-Allow-Headers', true);
-	  res.header('Access-Control-Allow-Credentials', true);
-	  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  next();
-  });
 
 app.use(
 	fileUpload({
