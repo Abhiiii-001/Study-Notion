2c2bb214088f532136cf2a5feec9bf18cfa781a9
Abhishek Kumar <114284738+Abhiiii-001@users.noreply.github.com>
2024-05-27 22:56:11 +0530

    Update Footer.jsx

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ CodeHelp © 2023 Studynotion</div>
+          <div className="text-center">Made with ❤️ Abhi © 2024 Ed-Circle</div>
         </div>
       </div>
     </div>
