ad76a727cfcf0d5e2dac9be440f41941f52345da
Abhiiii-001 <jaiswal2abhi1947@gmail.com>
2024-05-27 18:14:56 +0530

    fgs

---
+++ server/config/cloudinary.js
@@ -0,0 +1,20 @@
+const cloudinary = require("cloudinary").v2; //! Cloudinary is being required
+
+exports.cloudinaryConnect = () => {
+	try {
+		cloudinary.config({
+			//!    ########   Configuring the Cloudinary to Upload MEDIA ########
+			cloud_name: process.env.CLOUD_NAME,
+			api_key: process.env.API_KEY,
+			api_secret: process.env.API_SECRET,
+		})
+		// .then(() =>  console.log("Cloudinay Connected Successfully"))
+		// .catch( (error) => {
+		// 	 console.log("Cloudinay Connection Failed");
+		// 	console.error(error);
+		// 	process.exit(1);
+		// } )
+	} catch (error) {
+		 console.log(error);
+	}
+};
