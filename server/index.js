5d1eb3dbc100f7ee6341a76a6c8640321dd2ffa8
Abhishek Kumar <114284738+Abhiiii-001@users.noreply.github.com>
2024-05-27 18:37:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:["http://localhost:3000"],
+		origin:["https://ed-circle-pi.vercel.app"],
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		credentials:true,
 	})
