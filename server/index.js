1aef3beb2bd63e67c90c44c2af202bd46144cbbd
Abhishek Kumar <114284738+Abhiiii-001@users.noreply.github.com>
2024-05-27 19:51:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,8 +27,8 @@ app.use(
 	})
 )
 app.use((req, res, next) => {
-  res.headers.append('Access-Control-Allow-Origin', '*');
-  res.headers.append('Access-Control-Allow-Credentials', 'true');
+  res.headers('Access-Control-Allow-Origin', '*');
+  res.headers('Access-Control-Allow-Credentials', 'true');
   next();
 });
 app.use(express.json());
