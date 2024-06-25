8e260f8eca31319dc783cfc6e333fbba7e4f3247
Abhishek Kumar <114284738+Abhiiii-001@users.noreply.github.com>
2024-06-25 13:19:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:["https://ed-circle-pi.vercel.app"],
+		origin:["https://study-notion-learn.vercel.app"],
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		credentials:true,
 	})
